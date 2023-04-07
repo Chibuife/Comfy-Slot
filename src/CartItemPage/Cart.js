b53defa83ead1b944f191437d80b2623515a16ea
Chibuife John <chibuifejohn1@gmail.com>
2023-04-07 22:14:45 +0100

    complete cart ui

--- src/CartItemPage/Cart.js
+++ src/CartItemPage/Cart.js
@@ -15,16 +15,16 @@ const Cart = ({ myArray }) => {
       <section className="sectionOne">
         <table>
           <thead>
-            <tr>
-              <th>Item</th>
-              <th> Price</th>
-              <th>Quantity</th>
-              <th colSpan={2}>Subtotal</th>
+            <tr className="heading">
+              <th> <h5>Item </h5></th>
+              <th><h5>Price </h5></th>
+              <th><h5>Quantity</h5></th>
+              <th><h5>Subtotal</h5></th>
+              <th colSpan={6}></th>
             </tr>
           </thead>
           <tbody>
             {selector.map((item, index) => {
-              console.log(item.color)
               const add = () => dispatch(adding(item))
               const subtract = () => dispatch(subtracting(item))
               if (item.quantity > 0) {
@@ -34,13 +34,14 @@ const Cart = ({ myArray }) => {
                       <img src={item.image} alt="" />
                       <div>
                         <h4>{item.name}</h4>
-                        <span>Color: <button className={item.color}></button></span>
+                        <span className="colorLabel">Color: <button className={item.color}></button></span>
+                        <h5 className="price smallscr">$ {item.price} </h5>
                       </div>
                     </td>
-                    <td>${item.price}</td>
-                    <td className="numbeOfItems"><span onClick={subtract} className="count">-</span> <span>{item.quantity}</span> <span onClick={add} className="count">+</span></td>
-                    <td>$ {item.totalPrice.toFixed(2)}</td>
-                    <td > <button className="delete" ><MdDelete /></button></td>
+                    <td className="price largescr">$ {item.price}</td>
+                    <td className="numbeOfItems"><span onClick={subtract} className="count">-</span> <span className="number">{item.quantity}</span> <span onClick={add} className="count">+</span></td>
+                    <td className="totalValue">$ {item.totalPrice.toFixed(2)}</td>
+                    <td className="delCol"> <button className="delete" ><MdDelete /></button></td>
 
                   </tr>
                 )
@@ -50,10 +51,12 @@ const Cart = ({ myArray }) => {
         </table>
       </section>
       <div className="buttons">
-        <button>Continue Shopping</button>
-        <button>Clear Shopping Cart</button>
+        <button className="continue">Continue Shopping</button>
+        <button className="clear">Clear Shopping Cart</button>
       </div>
+      <div className="paymentSlot">
     <PaymentSlot/>
+      </div>
     </div>
   )
 }
--- src/CartItemPage/PaymentSlot.js
+++ src/CartItemPage/PaymentSlot.js
@@ -5,35 +5,31 @@ const PaymentSlot = () => {
     let total = 0;
 
     const itemsList = useSelector(state => state.itemsList)
-    // console.log
-    // useEffect(() => {
+   
     itemsList.forEach((item) => {
         console.log(item)
         total += item.totalPrice
         })
     console.log(total)
 
-    // }, [selector]
-    // )
-    // total = parseInt(selector.subTotal.toFixed(2))
+  
     return (
         <section className="sectionTwo">
-
-            {/* <div className="paymentSlot"> */}
+            <div>
             <table>
                 <tr>
                     <th>Subtotal:</th> <th>${total}</th>
                 </tr>
                 <tr className="shipping"><td>Shipping Fee</td> <td>$5.34</td>
                 </tr>
+                <div className="underline"></div>
                 <tr className="total">
                     <td>Order Total</td> <td>$36.33</td>
                 </tr>
-                <tr>
-                    <td colSpan={2}><button >LOGIN</button></td>
-                </tr>
             </table>
-            {/* </div> */}
+            <button className="login" >LOGIN</button>
+            </div>
+
         </section>
     )
 }
